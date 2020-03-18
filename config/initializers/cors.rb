76049cbbe717a902b467ed58dfdd8472552db2e6
ferdianafsa <farisafza201002@gmail.com>
2020-03-18 11:51:45 +0700

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,13 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-     origins '*'
-     resource '*', :headers => :any, :methods => [:get, :post, :options]
-   end
+    origins '*'
+    resource(
+      '/api/*',
+      headers: %w(Authorization),
+      methods: :any,
+      expose: ["Authorization"],
+      max_age: 600
+    )
+  end
 end# end of middleware
